560f37761a99f9f94f75412ce201e3b46ebdc438
Wesley Gonçalves <wesleylg01@gmail.com>
2019-09-27 15:52:05 -0300

    Ajustes Email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -18,7 +18,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'wesleylg01@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Fale conosco site | mensagem de :  $name";
-$email_body = "Mensagem via Fale conosco - wgltec.com.br .\n\n"."Detalhes:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "Mensagem via Fale conosco - wgltec.com.br .\n\n"."Detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nTelefone: $phone\n\nMenssagem:\n$message";
 $headers = "From: noreply@wgltec.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
